c62c45ed33f533132ca458edd57e19a6e6a36076
Juliana <cbatista.juliana@gmail.com>
2020-07-28 18:41:15 -0300

    react-slick

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,16 +3,17 @@ import Menu from '../../components/Menu'
 import BannerMain from '../../components/BannerMain'
 import Carousel from '../../components/Carousel'
 import dadosIniciais from '../../data/dados_iniciais'
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu />
 
-      <BannerMain 
+      <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end?"}
+        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
       />
 
       <Carousel
@@ -20,6 +21,27 @@ function Home() {
         category={dadosIniciais.categorias[0]}
       />
 
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
--- src/pages/cadastro/Cadegoria/index.js
+++ src/pages/cadastro/Cadegoria/index.js
@@ -5,11 +5,28 @@ import { Link } from 'react-router-dom';
 function CadastroCategoria() {
     return (
       <PageDefault>
-        <h1>Cadastro de Categoria </h1>
+         <h1>Cadastro de Categoria</h1>
+
+         <form>
+
+            <label>
+                Nome da Categoria:
+                <input
+                type="text"
+                />
+            </label>
+
+            <button>
+                Cadastrar
+            </button>
+
+        </form>
+
 
         <Link to="/">
-            Voltar
+            Ir para home
         </Link>
+
       </PageDefault>
     )
 }
