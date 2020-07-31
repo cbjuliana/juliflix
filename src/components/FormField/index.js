395836b2d0f9e123843eee15a0f3e52fa82651d4
Juliana <cbatista.juliana@gmail.com>
2020-07-31 18:34:52 -0300

    FormField

---
+++ src/components/FormField/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function FormField( { label, type, name, values, onChange } ) {
+    return (
+        <div>
+            <label>
+                {label}
+                <input
+                type={type}
+                name={name}
+                value={values}
+                onChange={onChange}
+                />
+            </label>
+         </div>
+    )
+}
+
+export default FormField;
